21459c4c2440aa440daa73cc2fd792d5f9fdc9fb
He2Henrique <134214073+He2Henrique@users.noreply.github.com>
2025-09-14 21:59:37 -0300

    TEST: Simple form data

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use App\controllers\SiteController;
 $app = new Application(__DIR__);
 
 $app->router->get('/home', [SiteController::class, 'home']);
+$app->router->post('/home', [SiteController::class, 'handleData']);
 // $app->router->post('/home', [SiteController::class, 'handleData' ]);
 
 $app->run();
